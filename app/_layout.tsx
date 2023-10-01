c70dc9318637801ed72e217d760b70316a874abc
ww8007 <wshmin1234@gmail.com>
2023-10-02 06:05:50 +0900

    feat: 로그인, email 창 라우팅 추가

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,56 +1,62 @@
-import FontAwesome from '@expo/vector-icons/FontAwesome';
-import { DarkTheme, DefaultTheme, ThemeProvider } from '@react-navigation/native';
-import { useFonts } from 'expo-font';
-import { SplashScreen, Stack } from 'expo-router';
-import { useEffect } from 'react';
-import { useColorScheme } from 'react-native';
+import {
+	DarkTheme,
+	DefaultTheme,
+	ThemeProvider,
+	createNavigationContainerRef
+} from "@react-navigation/native";
+import { SplashScreen, Stack } from "expo-router";
+import { useColorScheme } from "react-native";
+import useSettingFont from "../common/hooks/useFonts";
+import { AuthProvider } from "../auth/context/AuthContext";
+import { useEffect } from "react";
 
 export {
-  // Catch any errors thrown by the Layout component.
-  ErrorBoundary,
-} from 'expo-router';
+	// Catch any errors thrown by the Layout component.
+	ErrorBoundary
+} from "expo-router";
+
+interface NavigationParams {
+	index: undefined;
+	login: undefined;
+}
+
+export const navigationRef = createNavigationContainerRef<NavigationParams>();
 
 export const unstable_settings = {
-  // Ensure that reloading on `/modal` keeps a back button present.
-  initialRouteName: '(tabs)',
+	// Ensure that reloading on `/modal` keeps a back button present.
+	initialRouteName: "/login"
 };
 
 // Prevent the splash screen from auto-hiding before asset loading is complete.
 SplashScreen.preventAutoHideAsync();
 
 export default function RootLayout() {
-  const [loaded, error] = useFonts({
-    SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf'),
-    ...FontAwesome.font,
-  });
+	const { loaded } = useSettingFont();
 
-  // Expo Router uses Error Boundaries to catch errors in the navigation tree.
-  useEffect(() => {
-    if (error) throw error;
-  }, [error]);
+	useEffect(() => {
+		if (loaded) {
+			SplashScreen.hideAsync();
+		}
+	}, [loaded]);
 
-  useEffect(() => {
-    if (loaded) {
-      SplashScreen.hideAsync();
-    }
-  }, [loaded]);
+	if (!loaded) {
+		return null;
+	}
 
-  if (!loaded) {
-    return null;
-  }
-
-  return <RootLayoutNav />;
+	return <RootLayoutNav />;
 }
 
 function RootLayoutNav() {
-  const colorScheme = useColorScheme();
+	const colorScheme = useColorScheme();
 
-  return (
-    <ThemeProvider value={colorScheme === 'dark' ? DarkTheme : DefaultTheme}>
-      <Stack>
-        <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-        <Stack.Screen name="modal" options={{ presentation: 'modal' }} />
-      </Stack>
-    </ThemeProvider>
-  );
+	return (
+		<AuthProvider>
+			<ThemeProvider value={colorScheme === "dark" ? DarkTheme : DefaultTheme}>
+				<Stack screenOptions={{ headerShown: false }}>
+					<Stack.Screen name='login' options={{ headerShown: false }} />
+					<Stack.Screen name='email' options={{ animation: "flip" }} />
+				</Stack>
+			</ThemeProvider>
+		</AuthProvider>
+	);
 }
