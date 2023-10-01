5986934140415e9519c41e6529122be0f18b20a7
ww8007 <wshmin1234@gmail.com>
2023-10-02 05:02:33 +0900

    feat: 유저 firebase google, apple 로그인 기능 추가

---
+++ auth/hooks/useGetGoogleAuth.ts
@@ -0,0 +1,39 @@
+import analytics from "@react-native-firebase/analytics";
+import auth from "@react-native-firebase/auth";
+import { GoogleSignin } from "@react-native-google-signin/google-signin";
+import Constants from "expo-constants";
+import useSignInUser from "./useSignInUser";
+
+GoogleSignin.configure({
+	webClientId: Constants.expoConfig?.extra?.expoClientId || ""
+});
+
+const useGetGoogleAuth = () => {
+	const { signInUser } = useSignInUser();
+
+	const onClickGoogleLogin = async () => {
+		// Check if your device supports Google Play
+		await GoogleSignin.hasPlayServices({ showPlayServicesUpdateDialog: true });
+		// Get the users ID token
+		const { idToken } = await GoogleSignin.signIn();
+
+		// Create a Google credential with the token
+		const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+
+		// Sign-in the user with the credential
+		return auth()
+			.signInWithCredential(googleCredential)
+			.then(async (res) => {
+				const { user } = res;
+				const token = await user.getIdToken();
+				signInUser(token);
+				await analytics().logLogin({
+					method: "google"
+				});
+			});
+	};
+
+	return { onClickGoogleLogin };
+};
+
+export default useGetGoogleAuth;
