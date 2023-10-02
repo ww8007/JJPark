ce081bf5cea4cf4c803a7f850a3ab33444ebce18
ww8007 <wshmin1234@gmail.com>
2023-10-03 07:55:57 +0900

    feat: 설정 페이지 추가

---
+++ app/setting/index.tsx
@@ -0,0 +1,85 @@
+import React from "react";
+import { SafeAreaView, StyleSheet, View } from "react-native";
+import Header from "../../src/common/ui/Header";
+import Colors from "../../src/constants/Colors";
+import { TouchableHighlight } from "react-native-gesture-handler";
+import { Text } from "../../src/common/ui/Text";
+import { MaterialIcons } from "@expo/vector-icons";
+import { useRouter } from "expo-router";
+import auth from "@react-native-firebase/auth";
+
+const index = () => {
+	const router = useRouter();
+
+	const onClick_내정보수정 = () => {
+		router.push("/setting/profile");
+	};
+
+	const onClick_로그아웃 = () => {
+		auth().signOut();
+	};
+
+	const onClick_회원탈퇴 = () => {
+		auth().currentUser?.delete();
+	};
+
+	const onClick_문의하기 = () => {
+		// TODO: 문의하기
+	};
+
+	return (
+		<SafeAreaView style={styles.safeArea}>
+			<Header showBackButton title='설정' />
+			<View style={styles.contentWrapper}>
+				<ArrowTextItem text='내 정보 수정' onPress={onClick_내정보수정} />
+				<ArrowTextItem text='로그아웃' onPress={onClick_로그아웃} />
+				<ArrowTextItem text='회원탈퇴' onPress={onClick_회원탈퇴} />
+				<ArrowTextItem text='문의하기' onPress={onClick_문의하기} />
+			</View>
+		</SafeAreaView>
+	);
+};
+
+export default index;
+
+const styles = StyleSheet.create({
+	safeArea: {
+		flex: 1,
+		backgroundColor: Colors.light.background
+	},
+	contentWrapper: {
+		flex: 1,
+		gap: 20
+	}
+});
+
+interface ArrowTextItemProps {
+	text: string;
+	onPress: () => void;
+}
+
+const ArrowTextItem = ({ text, onPress }: ArrowTextItemProps) => {
+	return (
+		<TouchableHighlight onPress={onPress} underlayColor={Colors.light.grey700}>
+			<View style={arrowStyles.arrowTextItemWrapper}>
+				<Text style={arrowStyles.arrowTextItem}>{text}</Text>
+				<MaterialIcons name='keyboard-arrow-right' size={24} color='black' />
+			</View>
+		</TouchableHighlight>
+	);
+};
+
+const arrowStyles = StyleSheet.create({
+	arrowTextItemWrapper: {
+		flexDirection: "row",
+		justifyContent: "space-between",
+		alignItems: "center",
+		paddingHorizontal: 20,
+		paddingVertical: 15,
+		backgroundColor: Colors.light.grey200
+	},
+	arrowTextItem: {
+		fontSize: 16,
+		color: Colors.light.black
+	}
+});
