c5b2376e2a933a6edc280dcd2d6cd6c76431ead3
ww8007 <wshmin1234@gmail.com>
2023-10-04 04:49:22 +0900

    feat: 결과화면 Step 추가 및 이동하는 로직 추가

--- app/index.tsx
+++ app/index.tsx
@@ -1,16 +1,40 @@
 import useFunnel from "../src/common/hooks/useFunnel";
-import 신청화면 from "../src/components/신청화면";
+import 신청화면 from "../src/park/components/신청화면";
+import 결과화면 from "../src/park/components/결과화면";
+import { useEffect, useMemo } from "react";
+import useAuthContext from "../src/auth/hooks/useAuthContext";
+import { STATUS } from "../src/user/db/user";
 
 const index = () => {
-	const { Funnel } = useFunnel({
-		step: ["SUBMIT", 'SUCCESS" , "LOADING', "FAILURE"]
+	const { user } = useAuthContext();
+	const memoStep = useMemo(() => ["SUBMIT", "RESULT"], []);
+	const { Funnel, setStep } = useFunnel({
+		step: memoStep
 	});
+
+	const onClickSubmit = () => {
+		setStep("RESULT");
+	};
+
+	const onClickCancel = () => {
+		setStep("SUBMIT");
+	};
+
+	useEffect(() => {
+		if (!user) return;
+		if (user.status !== STATUS.NONE) {
+			setStep("RESULT");
+		}
+	}, [user]);
 
 	return (
 		<>
 			<Funnel>
 				<Funnel.Step name='SUBMIT'>
-					<신청화면 />
+					<신청화면 onNext={onClickSubmit} />
+				</Funnel.Step>
+				<Funnel.Step name='RESULT'>
+					<결과화면 onPrev={onClickCancel} />
 				</Funnel.Step>
 			</Funnel>
 		</>
