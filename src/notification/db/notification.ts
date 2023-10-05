1d06cda640a0c52f85bb7f87e75dc0b7c6e25f8e
ww8007 <wshmin1234@gmail.com>
2023-10-05 09:25:05 +0900

    feat: 일반 유저에게 보낼 FCM 트리거 메서드 추가

--- src/notification/db/notification.ts
+++ src/notification/db/notification.ts
@@ -1,4 +1,5 @@
 import firestore from "@react-native-firebase/firestore";
+import { STATUS, updateUser } from "../../user/db/user";
 
 const db = firestore();
 
@@ -16,6 +17,24 @@ export const sendToAdmin = async (uid: string, message: string) => {
 			uid,
 			createdAt: firestore.FieldValue.serverTimestamp(),
 			content: message
+		});
+	} catch (error) {
+		throw error;
+	}
+};
+
+type UserMessage = "신청이 승인되었습니다" | "신청이 거절되었습니다";
+
+export const sendToUser = async (uid: string, message: UserMessage) => {
+	try {
+		await db.collection("userNotification").add({
+			uid,
+			createdAt: firestore.FieldValue.serverTimestamp(),
+			content: message
+		});
+		const user = updateUser(uid, {
+			status:
+				message === "신청이 승인되었습니다" ? STATUS.APPROVED : STATUS.REJECTED
 		});
 	} catch (error) {
 		throw error;
